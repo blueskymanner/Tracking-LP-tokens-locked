10baef2746e4d3e573feec228d9d07239478b0a0
bluskymanner <page111first@gmail.com>
2022-02-11 17:37:45 +0100

    modified files.

--- client/src/Header/index.js
+++ client/src/Header/index.js
@@ -4,11 +4,15 @@ function Header() {
     return (
         <div className="header">
             <div className="navbar">
-                <a href="#" className="active left" > <img src="/assets/images/Lock.png" alt='' height="30" width="30" /> </a> 
-                <a href="#" className="logo left"> <b> Track Locker </b> </a> 
-                <a href="#" className="right"> <img src="/assets/images/medium.svg" alt='' height="20" width="20" /> </a> 
-                <a href="#" className="right"> <img src="/assets/images/twitter.svg" alt='' height="20" width="20" /> </a>
-                <a href="#" className="right"> <img src="/assets/images/telegram.svg" alt='' height="20" width="20" /> </a>
+                <div className="leftbar">
+                    <a href="#" className="active left" > <img src="/assets/images/Lock.png" alt='' height="30" width="30" /> </a> 
+                    <a href="#" className="logo left"> <b> Track Locker </b> </a> 
+                </div>
+                <div className="rightbar">
+                    <a href="#" className="right"> <img src="/assets/images/medium.svg" alt='' height="20" width="20" /> </a> 
+                    <a href="#" className="right"> <img src="/assets/images/twitter.svg" alt='' height="20" width="20" /> </a>
+                    <a href="#" className="right"> <img src="/assets/images/telegram.svg" alt='' height="20" width="20" /> </a>
+                </div>
             </div>
         </div>
     );
--- client/src/Table/index.js
+++ client/src/Table/index.js
@@ -152,60 +152,64 @@ function Actiontable({ columns, data, pageNo, setPageIndex }) {
         </div>
 
         <div className="pagination">
-          <button onClick={() => {
-            gotoPage(0);
-            setPageIndex(0);
-          }} disabled={!canPreviousPage}>
-            {"<<"}
-          </button>{" "}
-          <button onClick={() => {
-            previousPage()
-            setPageIndex(pageIndex - 1);
-          }} disabled={!canPreviousPage}>
-            {"<"}
-          </button>{" "}
-          <button onClick={() => {
-            nextPage()
-            setPageIndex(pageIndex + 1);
-          }} disabled={!canNextPage}>
-            {">"}
-          </button>{" "}
-          <button onClick={() => {
-            gotoPage(pageCount - 1)
-            setPageIndex(pageCount - 1);
-          }} disabled={!canNextPage}>
-            {">>"}
-          </button>{" "}
-          <span>
-            Page{" "}
-            <strong>
-              {pageIndex + 1} of {pageCount}
-            </strong>{" "}
-          </span>
-          <span>
-            | Go to page:{" "}
-            <input
-              type="number"
-              defaultValue={pageIndex + 1}
+          <div className="leftpagebar">
+            <button onClick={() => {
+              gotoPage(0);
+              setPageIndex(0);
+            }} disabled={!canPreviousPage}>
+              {"<<"}
+            </button>{" "}
+            <button onClick={() => {
+              previousPage()
+              setPageIndex(pageIndex - 1);
+            }} disabled={!canPreviousPage}>
+              {"<"}
+            </button>{" "}
+            <button onClick={() => {
+              nextPage()
+              setPageIndex(pageIndex + 1);
+            }} disabled={!canNextPage}>
+              {">"}
+            </button>{" "}
+            <button onClick={() => {
+              gotoPage(pageCount - 1)
+              setPageIndex(pageCount - 1);
+            }} disabled={!canNextPage}>
+              {">>"}
+            </button>{" "}
+            <span>
+              Page{" "}
+              <strong>
+                {pageIndex + 1} of {pageCount}
+              </strong>{" "}
+            </span>
+          </div>
+          <div className="righttpagebar">
+            <span>
+              Go to page:{" "}
+              <input
+                type="number"
+                defaultValue={pageIndex + 1}
+                onChange={(e) => {
+                  const page = e.target.value ? Number(e.target.value) - 1 : 0;
+                  gotoPage(page);
+                }}
+                style={{ width: "70px" }}
+              />
+            </span>{" "}
+            <select
+              value={pageSize}
               onChange={(e) => {
-                const page = e.target.value ? Number(e.target.value) - 1 : 0;
-                gotoPage(page);
+                setPageSize(Number(e.target.value));
               }}
-              style={{ width: "100px" }}
-            />
-          </span>{" "}
-          <select
-            value={pageSize}
-            onChange={(e) => {
-              setPageSize(Number(e.target.value));
-            }}
-          >
-            {[10, 20, 30, 40].map((pageSize) => (
-              <option key={pageSize} value={pageSize}>
-                Show {pageSize}
-              </option>
-            ))}
-          </select>
+            >
+              {[10, 20, 30, 40].map((pageSize) => (
+                <option key={pageSize} value={pageSize}>
+                  Show {pageSize}
+                </option>
+              ))}
+            </select>
+          </div>
         </div>
       </div>
     </>
